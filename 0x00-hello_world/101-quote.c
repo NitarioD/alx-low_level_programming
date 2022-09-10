ebb2c3f58fe2b7797c1be33cbf93f7316e533fce
Jola <nitariod@gmail.com>
2022-09-10 14:15:32 -0700

    0x01 optional task8 hot fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main- Entry point
@@ -7,5 +8,6 @@
  */
 int main(void)
 {
-	return(0);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return(1);
 }

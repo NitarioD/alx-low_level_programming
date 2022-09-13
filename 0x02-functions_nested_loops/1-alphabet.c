b88ad5bf87008590c94f3fbe698f2a7e095d2341
Jola <nitariod@gmail.com>
2022-09-13 07:43:23 -0700

    0x02 task1 and update main.h

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for(i = 97; i < 123; i++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}

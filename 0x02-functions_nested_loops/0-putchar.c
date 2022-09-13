19182264635dcf420f6a9e2907d0482fdba6ad0a
Jola <nitariod@gmail.com>
2022-09-13 06:26:33 -0700

    0x02 task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main- Entry point
+ *
+ * Description: prinnts array of char to screen
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int stringArr[9] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(stringArr[i]);
+	}
+	_putchar('\n');
+	return (0);
+}

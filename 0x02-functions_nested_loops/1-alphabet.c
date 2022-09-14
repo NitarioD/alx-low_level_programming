d73e953fdcedd9019eae8c584104de9e65a9b1e0
Jola <nitariod@gmail.com>
2022-09-13 18:18:53 -0700

    test description

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
-/**
- * main - check the code
- *
- * Description: print out all the alphabets in lower case
- *
- * Return: Always 0.
- */
 void print_alphabet(void)
 {
+	/**
+ 	* main - check the code
+ 	*
+ 	* Description: print out all the alphabets in lower case
+ 	*
+ 	* Return: Always 0.
+ 	*/
 	int i;
 
 	for (i = 97; i < 123; i++)

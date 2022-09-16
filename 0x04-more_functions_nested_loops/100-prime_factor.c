bba0d923cdbf312911fa1f3ddc71be216419cb7e
Jola <nitariod@gmail.com>
2022-09-16 13:45:37 -0700

    0x04 100 attempt 2

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,15 +7,14 @@
  */
 int main(void)
 {
-	unsigned long long int number = 612852475143;
+	unsigned long long number = 612852475143;
 
-	unsigned long long int i;
+	unsigned long long i;
 
 	for (i = 2; i <= number; i++)
 	{
 		while ((number % i) == 0 && number != i)
 		{
-			/*printf("%llu, ", i);*/
 			number /= i;
 		}
 	}

809da650906478702b104159c9166b3ab38d2cfc
Jola <nitariod@gmail.com>
2022-10-04 17:10:48 -0700

    0x0B 100 attempt 1

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * argstostr - function that concatenates all the arguments
+ * of your program.
+ *
+ * @ac: arg count
+ * @av: arg vector
+ * Return: pointer
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j, width, size;
+	char *arr;
+
+	j = 0;
+	size = 0;
+	for (i = 1; i < ac; i++)
+	{
+		width = 0;
+		while (av[i][width] != '\0')
+		{
+			size++;
+			width++;
+		}
+		size++;
+	}
+	size += ac - 1;
+	arr = (char *)malloc((size + 1) * sizeof(char *));
+	if (arr == NULL)
+		return (NULL);
+	for (i = 1; i < ac; i++)
+	{
+		width = 0;
+		while (av[i][width] != '\0')
+		{
+			arr[j] = av[i][width];
+			j++;
+			width++;
+		}
+		arr[j] = '\n';
+		j++;
+	}
+	arr[size + 1] = '\0';
+	return (arr);
+}

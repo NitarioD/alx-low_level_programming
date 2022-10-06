f3f3a6592234e0c0981374f323deeb13114969df
Jola <nitariod@gmail.com>
2022-10-06 07:31:02 -0700

    0x0C 2 initialize memory spaces to 0

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,13 +10,15 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *alloc;
-	int i = 0;
+	char *alloc;
+	unsigned int i = 0;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	alloc = malloc(nmemb * size);
 	if (alloc == NULL)
 		return (NULL);
+	for (i = 0; i < nmemb * size; i++)
+		alloc[i] = 0;
 	return (alloc);
 }

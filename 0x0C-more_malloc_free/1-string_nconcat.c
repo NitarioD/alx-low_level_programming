a9d2fee88382ef0ed80def3eec504eace4a92486
Jola <nitariod@gmail.com>
2022-10-06 14:26:33 -0700

    0x0C 1 hot fix 3

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,47 +1,40 @@
 #include "main.h"
-
 /**
- * string_nconcat - function that concatenates two strings.
+ * string_nconcat - concatenates two strings
  *
- * @s1: string 1
- * @s2: string 2
- * @n: number of char to copy
- * Return: char pointer
- */
+ * @s1: first string
+ * @s2: second string
+ * @n: limit of s2
+ * Return: pointer to new space in memory or null
+ **/
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *cpy;
 	int i;
 	unsigned int j;
 
+	i = 0;
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 	while (s1[i] != '\0')
-	{
-		size1++;
 		i++;
-	}
-	i = 0;
-	while (s2[i] != '\0')
-	{
-		size2++;
-		i++;
-	}
-	if (n < size2)
-		size2 = n;
-	ttLen = size1 + size2;
-	concat = malloc(sizeof(char) * ttLen);
-	if (concat == NULL)
+	cpy = malloc(sizeof(char) * (i + n + 1));
+	if (cpy == NULL) 
 		return (NULL);
 	i = 0;
-	for (j = 0; j < size1; j++)
-		concat[j] = s1[j];
-	i = j;
-	for (j = 0; j < size2; j++)
-		concat[i + j] = s2[j];
-	concat[ttLen] = '\0';
-	return (concat);
->>>>>>> 0cd5b998fc9a3a8fe269d02a703d62f98a5e874e
+	j = 0;
+	while (s1[i] != '\0')
+	{
+		cpy[i] = s1[i];
+		i++;
+	}
+	while (j < n && s2[j] != '\0')
+	{
+		cpy[i] = s2[j];
+		i++, j++;
+	}
+	cpy[i] = '\0';
+	return (cpy);
 }

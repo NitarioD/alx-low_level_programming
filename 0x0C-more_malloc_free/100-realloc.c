05bd0044080fdec270dda537efe6c8f1e4f2c37e
Jola <nitariod@gmail.com>
2022-10-07 04:57:19 -0700

    0x0C 100 attempt 3

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,7 +11,7 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *new_space;
-	int i;
+unsigned int i;
 	char *ptrCpy = (char *)ptr;
 	if (new_size == old_size)
 		return (ptr);
